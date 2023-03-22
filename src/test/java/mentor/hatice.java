e663522b634ffbcdfb41b29028554030fc43b46f
hatice kahraman <102755997+H-5563@users.noreply.github.com>
2023-03-22 14:20:00 +0000

    second day

--- src/test/java/mentor/hatice.java
+++ src/test/java/mentor/hatice.java
@@ -1,2 +1,8 @@
-package mentor;public class hatice {
+package mentor;
+
+public class hatice {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("second day");
+    }
 }
